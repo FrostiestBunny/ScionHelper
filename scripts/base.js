d850b5c96676c3ecd82d26e724cd1422a59d18c4
ZackUnfair <acezackfair@gmail.com>
2021-02-09 18:37:35 +0100

    Adding json stuff

---
+++ scripts/base.js
@@ -0,0 +1,24 @@
+class Character {
+    constructor(name, str, agi, vit, mag, lck, hp, mana, soak, fp) {
+        this.name = name;
+        this.str = str;
+        this.agi = agi;
+        this.vit = vit;
+        this.mag = mag;
+        this.lck = lck;
+        this.hp = hp;
+        this.mana = mana;
+        this.soak = soak;
+        this.fp = fp;
+    }
+}
+
+window.onload = (event) => {
+    loadCharacters(window.location.origin + "/data/PCs.json");
+};
+
+const loadCharacters = (filename) => {
+    fetch(filename)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
